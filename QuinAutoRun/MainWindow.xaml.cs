f54e6de537eb548e9dcacc2d32a2465d217db4e0
Brian <bdev@btro.jp>
2025-02-05 20:53:37 +0900

    Added toggle to disable tool itself
    F11 will not turn off program so the user can freely use the Middle Mouse Button in-game for other needs.

--- QuinAutoRun/MainWindow.xaml.cs
+++ QuinAutoRun/MainWindow.xaml.cs
@@ -12,18 +12,27 @@ namespace QuinAutoRun
     {
         private const int HotkeyId = 9000;
         private bool isRunning = false;
+        private bool isListening = true;
         private Stopwatch toggleStopwatch = new Stopwatch();
-        private LowLevelMouseProc _proc;
-        private IntPtr _hookID = IntPtr.Zero;
+        private LowLevelMouseProc _mouseProc;
+        private LowLevelKeyboardProc _keyboardProc;
+        private IntPtr _mouseHookID = IntPtr.Zero;
+        private IntPtr _keyboardHookID = IntPtr.Zero;
 
         private const int WH_MOUSE_LL = 14;
+        private const int WH_KEYBOARD_LL = 13;
         private const int WM_MBUTTONDOWN = 0x0207;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int VK_F11 = 0x7A;
         private const int VK_SHIFT = 0x10;
         private const int VK_W = 0x57;
         private const uint KEYEVENTF_KEYUP = 0x0002;
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
@@ -43,21 +52,24 @@ namespace QuinAutoRun
             InitializeComponent();
             Loaded += MainWindow_Loaded;
             Closing += MainWindow_Closing;
-            _proc = HookCallback;
+            _mouseProc = MouseHookCallback;
+            _keyboardProc = KeyboardHookCallback;
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            _hookID = SetHook(_proc);
+            _mouseHookID = SetMouseHook(_mouseProc);
+            _keyboardHookID = SetKeyboardHook(_keyboardProc);
             toggleStopwatch.Start();
         }
 
         private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            UnhookWindowsHookEx(_hookID);
+            UnhookWindowsHookEx(_mouseHookID);
+            UnhookWindowsHookEx(_keyboardHookID);
         }
 
-        private IntPtr SetHook(LowLevelMouseProc proc)
+        private IntPtr SetMouseHook(LowLevelMouseProc proc)
         {
             using (Process curProcess = Process.GetCurrentProcess())
             using (ProcessModule curModule = curProcess.MainModule)
@@ -66,11 +78,21 @@ namespace QuinAutoRun
             }
         }
 
-        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
-
-        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        private IntPtr SetKeyboardHook(LowLevelKeyboardProc proc)
         {
-            if (nCode >= 0 && wParam == (IntPtr)WM_MBUTTONDOWN)
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private IntPtr MouseHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_MBUTTONDOWN && isListening)
             {
                 if (toggleStopwatch.ElapsedMilliseconds >= 500)
                 {
@@ -78,7 +100,21 @@ namespace QuinAutoRun
                     toggleStopwatch.Restart();
                 }
             }
-            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+            return CallNextHookEx(_mouseHookID, nCode, wParam, lParam);
+        }
+
+        private IntPtr KeyboardHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                if (vkCode == VK_F11)
+                {
+                    isListening = !isListening;
+                    UpdateStatus(isListening ? "LISTENING" : "NOT LISTENING", isListening ? Colors.Green : Colors.Gray);
+                }
+            }
+            return CallNextHookEx(_keyboardHookID, nCode, wParam, lParam);
         }
 
         private void ToggleKeyHolding()
