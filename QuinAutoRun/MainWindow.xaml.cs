04ccf0339bd3a153e0651e829d6616175638d56f
Brian <bdev@btro.jp>
2025-02-05 20:48:14 +0900

    Minor updates

--- QuinAutoRun/MainWindow.xaml.cs
+++ QuinAutoRun/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Interop;
 using System.Windows.Media;
 using System.Threading;
 
